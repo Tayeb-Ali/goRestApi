4421aa3c0f1b0fe287aa0192496479760cd25573
Tayeb-Ali <Mr.elteb@gmail.com>
2019-08-31 10:24:33 +0200

    add update method

--- main.go
+++ main.go
@@ -2,41 +2,42 @@ package main
 
 import (
 	"encoding/json"
-	"log"
-	"net/http"
-	"math/rand"
-	"strconv"
 	"github.com/gorilla/mux"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
 )
 
 //Book Stuct (Model)
 type Book struct {
-	ID 		 string  `json:"id"`
-	Isbn     string  `json:"isbn"`
-	Title	 string  `json:"title"`
-	Author	 *Author `json:"author"`
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
 }
 
 // Author Struct
 type Author struct {
-	Firstname		string		`json:"firstname"`
-	Lastname		string		`json:"lastname"`
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
 }
 
 //Init books var as a sclice book struct
 var books []Book
 
 // Get All Books
-func getBooks(w  http.ResponseWriter, r *http.Request) {
+func getBooks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(books)
 }
+
 // Get Single Book
-func getBook(w  http.ResponseWriter, r *http.Request) {
+func getBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params:= mux.Vars(r) //Get Params
+	params := mux.Vars(r) //Get Params
 	//Loop through books and find with id
-	for _, item:= range books {
+	for _, item := range books {
 		if item.ID == params["id"] {
 			json.NewEncoder(w).Encode(item)
 			return
@@ -44,8 +45,9 @@ func getBook(w  http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode(&Book{})
 }
+
 // Create a New Books
-func createBook(w  http.ResponseWriter, r *http.Request) {
+func createBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var book Book
 	_ = json.NewDecoder(r.Body).Decode(&book)
@@ -53,26 +55,39 @@ func createBook(w  http.ResponseWriter, r *http.Request) {
 	books = append(books, book)
 	json.NewEncoder(w).Encode(book)
 }
+
 // Update  Books
-func updateBook(w  http.ResponseWriter, r *http.Request) {
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"]
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
 
 }
+
 // Get All Books
-func deleteBook(w  http.ResponseWriter, r *http.Request) {
+func deleteBook(w http.ResponseWriter, r *http.Request) {
 
 }
 
-
-
-func main()  {
+func main() {
 	//Init Router
-	r:= mux.NewRouter()
+	r := mux.NewRouter()
 
 	// Mock Data - @todo - implement DB
 	books = append(books, Book{ID: "1", Isbn: "324245", Title: "Book One", Author: &Author{Firstname: "Elteyab", Lastname: "Ali"}})
-books = append(books, Book{ID: "2", Isbn: "80823", Title: "Book Two", Author: &Author{Firstname: "Hassan", Lastname: "Omer "}})
-books = append(books, Book{ID: "3", Isbn: "86788", Title: "Book Three", Author: &Author{Firstname: "Omer", Lastname: "Khalid "}})
-books = append(books, Book{ID: "3", Isbn: "1234567", Title: "Book Fore", Author: &Author{Firstname: "Mudether", Lastname: "Ghade "}})
+	books = append(books, Book{ID: "2", Isbn: "80823", Title: "Book Two", Author: &Author{Firstname: "Hassan", Lastname: "Omer "}})
+	books = append(books, Book{ID: "3", Isbn: "86788", Title: "Book Three", Author: &Author{Firstname: "Omer", Lastname: "Khalid "}})
+	books = append(books, Book{ID: "3", Isbn: "1234567", Title: "Book Fore", Author: &Author{Firstname: "Mudether", Lastname: "Ghade "}})
 
 	//Route Handlers /Endpoints
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
